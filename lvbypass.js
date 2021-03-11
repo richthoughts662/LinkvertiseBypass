e166cc649c6b5476ccec5cc1b094df2a5c29821f
Yuh <75444356+yuhsecurity@users.noreply.github.com>
2021-03-11 16:18:26 -0600

    Update to 0.4.3

--- lvbypass.js
+++ lvbypass.js
@@ -1,93 +1,70 @@
 // ==UserScript==
 // @name         Linkvertise Bypass
 // @namespace    http://tampermonkey.net/
-// @version      0.4.2
+// @version      0.4.3
 // @description  Bypass links that cannot be bypassed by Universal Bypass
 // @author       FireMasterK
 // @match        *://*.linkvertise.com/*
 // @match        *://*.linkvertise.net/*
 // @match        *://*.link-to.net/*
 // @match        *://*.linkvertise.download/*
+// @match        *://*.file-link.net/*
+// @match        *://*.direct-link.net/*
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
-
+ 
 var url = window.location.href.toString();
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-sleep(2000);
+ 
 if (url.indexOf("?r=") != -1) {
-  window.location = atob(decodeURIComponent(url.substr(url.indexOf("?r=") + 3)));
+    window.location = atob(decodeURIComponent(url.substr(url.indexOf("?r=") + 3)));
 } else {
-
-  // iframe check
-  if (window.parent.location != window.location) { return }
-
-  GM.xmlHttpRequest({
-    method: "GET",
-    headers: {
-      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
-    },
-    url: "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/captcha",
-    onload: function () { }
-  });
-  GM.xmlHttpRequest({
-    method: "GET",
-    headers: {
-      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
-    },
-    url: "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/countdown_impression?trafficOrigin=network",
-    onload: function () { }
-  });
-  GM.xmlHttpRequest({
-    method: "GET",
-    headers: {
-      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
-    },
-    url: "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/todo_impression?mobile=true&trafficOrigin=network",
-    onload: function () { }
-  });
-  GM.xmlHttpRequest({
-    method: "GET",
-    headers: {
-      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
-    },
-    url: "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/click?trafficOrigin=network",
-    onload: function () { }
-  });
-
-  let o = {
-    timestamp: new Date().getTime(),
-    random: "6548307"
-  };
-  var bypass_url = "https://publisher.linkvertise.com/api/v1/redirect/link/static" + window.location.pathname;
-  GM.xmlHttpRequest({
-    method: "GET",
-    headers: {
-      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
-    },
-    url: bypass_url,
-    onload: function (response) {
-      var json = JSON.parse(response.responseText);
-      o.link_id = json.data.link.id
-      bypass_url = "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/target?serial=" + encodeURIComponent(btoa(JSON.stringify(o)));
-
-      GM.xmlHttpRequest({
+ 
+    // iframe check
+    if (window.parent.location != window.location) { return }
+ 
+    var paths = ["/captcha", "/countdown_impression?trafficOrigin=network", "/todo_impression?mobile=true&trafficOrigin=network"]
+ 
+    paths.map(path => {
+        GM.xmlHttpRequest({
+            method: "GET",
+            headers: {
+                "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
+            },
+            url: "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + path
+        });
+    })
+ 
+ 
+    let o = {
+        timestamp: new Date().getTime(),
+        random: "6548307"
+    };
+    var bypass_url = "https://publisher.linkvertise.com/api/v1/redirect/link/static" + window.location.pathname;
+    GM.xmlHttpRequest({
         method: "GET",
         headers: {
-          "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
         },
         url: bypass_url,
         onload: function (response) {
-          var json = JSON.parse(response.responseText);
-          window.location = json.data.target;
-
+            var json = JSON.parse(response.responseText);
+            o.link_id = json.data.link.id
+            o = { serial: btoa(JSON.stringify(o)) }
+            bypass_url = "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/target?X-Linkvertise-UT=" + localStorage.getItem("X-LINKVERTISE-UT");
+ 
+            GM.xmlHttpRequest({
+                method: "POST",
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1",
+                    "Content-Type": "application/json"
+                },
+                data: JSON.stringify(o),
+                url: bypass_url,
+                onload: function (response) {
+                    var json = JSON.parse(response.responseText);
+                    window.location = json.data.target;
+                }
+            });
         }
-      });
-    }
-  });
+    });
 }
